9559c9c230195383fdb1761c3655dee3c5e1bd8d
Zemichael Yadeta <yetikimitabeba@gmail.com>
2024-03-22 03:22:30 +0300

    Defining stored procedure.

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,7 +1,7 @@
 -- A SQL script that creates a stored procedure AddBonus that adds a new
 -- correction for a student.
 
-DELIMITER //
+/* DELIMITER //
 
 CREATE PROCEDURE AddBonus(user_id INT, project_name VARCHAR(255), score INT)
 BEGIN
@@ -28,6 +28,27 @@ BEGIN
 
     INSERT INTO corrections (user_id, project_id, score)
     VALUES (user_id, p_id, score);
+END //
+
+DELIMITER ; */
+
+DELIMITER //
+
+CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
+BEGIN
+    DECLARE project_id INT;
+    
+    -- Check if the project exists
+    SELECT id INTO project_id FROM projects WHERE name = project_name;
+    
+    -- If project doesn't exist, create it
+    IF project_id IS NULL THEN
+        INSERT INTO projects (name) VALUES (project_name);
+        SET project_id = LAST_INSERT_ID();
+    END IF;
+    
+    -- Add correction
+    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
 END //
 
 DELIMITER ;
