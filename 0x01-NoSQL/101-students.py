8f7033fa819fc075c854f448899ab0eac018df32
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-03-27 02:44:30 +0300

    sorting by average score.

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+A Python function that returns all students sorted by average score.
+"""
+
+
+def top_students(mongo_collection):
+    """
+    A Python function that returns all students sorted by average score.
+    """
+    return mongo_collection.aggregate(
+        [
+            {"$project": {"name": "$name", "averageScore": {"$avg": "$topics.score"}}},
+            {"$sort": {"averageScore": -1}},
+        ]
+    )
